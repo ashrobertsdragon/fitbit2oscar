e9f9986cc129abc6838793d582d81c3e4f9d0204
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-02 22:35:54 -0700

    Add sleep data parsing

--- src/fitbit2oscar/extract.py
+++ src/fitbit2oscar/extract.py
@@ -1,9 +1,16 @@
 import datetime
 from collections.abc import Generator
 from pathlib import Path
+from typing import TypeAlias
 from zoneinfo import ZoneInfo
 
 import fitbit2oscar.read_file as read_file
+
+
+SleepSummary: TypeAlias = dict[str, dict[str, int]]
+SleepData: TypeAlias = list[dict[str, str | int]]
+SleepLevels: TypeAlias = dict[str, SleepSummary | SleepData]
+SleepEntry: TypeAlias = dict[str, str | int | SleepLevels]
 
 
 def convert_timestamp(
@@ -95,7 +102,198 @@ def extract_bpm_data(
         yield timestamp, bpm
 
 
+def is_valid_sleep_entry(
+    sleep_entry: SleepEntry,
+    start_date: datetime.date = datetime.date.fromordinal(1),
+    end_date: datetime.date = datetime.date.today(),
+) -> bool:
+    """
+    Validates a sleep entry based on format, date range, and presence of light
+    sleep data.
+
+    Args:
+        sleep_entry (SleepEntry): A dictionary containing sleep data with
+            keys for 'data', 'dateOfSleep', and 'levels'.
+        start_date (datetime.date, optional): The earliest date for a valid
+            sleep entry. Defaults to the earliest representable date.
+        end_date (datetime.date, optional): The latest date for a valid sleep
+            entry. Defaults to today's date.
+
+    Returns:
+        bool: True if the sleep entry is valid, False otherwise.
+    """
+
+    def is_valid_format() -> bool:
+        return all(
+            key in sleep_entry for key in ("data", "dateOfSleep", "levels")
+        )
+
+    is_valid_start: bool = (
+        start_date
+        <= datetime.date.fromisoformat(sleep_entry["dateOfSleep"])
+        <= end_date
+    )
+
+    return (
+        is_valid_format()
+        and is_valid_start
+        and "light" in sleep_entry["levels"]["summary"].keys()
+    )
+
+
+def extract_sleep_time_data(
+    json_entries: Generator[SleepEntry],
+    timezone: str,
+) -> Generator[SleepEntry, None, None]:
+    """
+    Extracts timestamp, duration, levels, start time, stop time, wake after
+    sleep onset duration, and sleep efficiency from JSON entries.
+
+    Creates a generator of dictionaries containing the relevant sleep data
+    from JSON entries.
+
+    Args:
+        json_entries (Generator[SleepEntry]): Generator of JSON entries of
+            sleep data in format of: {
+                "data": list[dict[str, str | int]],
+                "dateOfSleep": str,
+                "levels": dict[str, dict[str, dict[str, int]]]
+            }
+        timezone (str): Timezone to convert timestamps to.
+
+    Returns:
+        Generator[SleepEntry, None, None]: Generator of dictionaries
+            containing timestamp, duration, levels, start time, stop time,
+            wake after sleep onset duration, and sleep efficiency.
+    """
+
+    for entry in json_entries:
+        if is_valid_sleep_entry(entry):
+            entry_dict = {
+                "timestamp": entry["dateOfSleep"],
+                "duration": entry["duration"],
+                "levels": entry["levels"],
+                "start_time": convert_timestamp(entry["startTime"], timezone),
+                "stop_time": convert_timestamp(entry["endTime"], timezone),
+                "wake_after_sleep_onset_duration": entry["minutesAwake"],
+                "sleep_efficiency": entry["efficiency"],
+            }
+
+            yield entry_dict
+
+
+def convert_time_data(minutes: int) -> str:
+    """Converts time in minutes to a string in HH:MM:SS format."""
+    hours, mins = divmod(minutes, 60)
+    return f"{hours:02d}:{mins:02d}:00"
+
+
+def generate_hypnogram(data: SleepData) -> list[str]:
+    """
+    Generates a hypnogram from sleep data.
+
+    This function takes a list of sleep data entries, each consisting of a
+    timestamp and a sleep level with its duration in seconds, and converts it
+    into a hypnogram. A hypnogram is a list of sleep stages, where each stage
+    is repeated for every 30-second interval within its duration. The sleep
+    stages are mapped to specific labels: "WAKE", "REM", "Light", and "Deep".
+
+    Args:
+        data (SleepData): A list of dictionaries, each containing a "level"
+            key indicating the sleep stage and a "seconds" key representing
+            the duration of that stage.
+
+    Returns:
+        list[str]: A list of sleep stage names corresponding to each 30-second
+        interval in the input data.
+    """
+
+    levels = {"wake": "WAKE", "rem": "REM", "light": "Light", "deep": "Deep"}
+    sleep_stages = []
+
+    sleep_stages.extend(
+        [levels[stage["level"]]] * (stage["seconds"] // 30)
+        for stage in data
+        if stage["level"] in levels
+    )
+
+    return sleep_stages
+
+
+def parse_sleep_data(
+    sleep_data: SleepEntry,
+) -> dict[str, datetime.datetime | int]:
+    """
+    Parses sleep data into a structured dictionary format.
+
+    This function takes raw sleep data from a Fitbit sleep entry and converts
+    it into a dictionary with detailed sleep metrics. The returned dictionary
+    includes sleep onset duration, wake after sleep onset duration, durations
+    for light, deep, and REM sleep, number of awakenings, sleep efficiency,
+    start and stop times, and a hypnogram representing sleep stages.
+
+    Args:
+        sleep_data (SleepEntry): A dictionary containing raw sleep data with
+            keys for 'duration', 'levels', 'startTime', 'endTime',
+            'wake_after_sleep_onset_duration', and 'sleep_efficiency'.
+
+    Returns:
+        dict[str, datetime.datetime | int]: A dictionary containing parsed
+            sleep metrics, including durations in HH:MM:SS format, start and
+            stop times as datetime objects, and a hypnogram as a list of sleep
+            stage names.
+    """
+
+    return {
+        "sleep_onset_duration": convert_time_data(
+            sleep_data["duration"] / 60000
+        ),
+        "wake_after_sleep_onset_duration": convert_time_data(
+            sleep_data["wake_after_sleep_onset_duration"]
+        ),
+        "light_sleep_duration": convert_time_data(
+            sleep_data["levels"]["summary"]["light"]["minutes"]
+        ),
+        "deep_sleep_duration": convert_time_data(
+            sleep_data["levels"]["summary"]["deep"]["minutes"]
+        ),
+        "rem_sleep_duration": convert_time_data(
+            sleep_data["levels"]["summary"]["rem"]["minutes"]
+        ),
+        "number_awakenings": sleep_data["levels"]["summary"]["wake"]["count"],
+        "sleep_efficiency": sleep_data["sleep_efficiency"],
+        "start_time": sleep_data["start_time"],
+        "stop_time": sleep_data["stop_time"],
+        "hypnogram": generate_hypnogram(sleep_data["levels"]["data"]),
+    }
+
+
 def extract_sleep_data(
+    sleep_files: list[Path],
+    timezone: str,
+) -> list[dict[str, datetime.datetime | int]]:
+    """
+    Extracts sleep data from a list of Fitbit sleep JSON files and timezone.
+
+    Args:
+        sleep_files (list[Path]): List of JSON files containing sleep data.
+        timezone (str): Timezone to convert timestamps to.
+
+    Returns:
+        list[dict[str, datetime.datetime | int]]: List of sleep data
+            dictionaries containing sleep onset duration, wake after sleep
+            onset duration, light sleep duration, deep sleep duration, REM
+            sleep duration, number of awakenings, sleep efficiency, start
+            time, stop time, and hypnogram.
+    """
+
+    return [
+        parse_sleep_data(extract_sleep_time_data(json_entry), timezone)
+        for json_entry in read_file.read_json_file(sleep_files)
+    ]
+
+
+def extract_sleep_health_data(
     sp02_files: list[Path],
     bpm_files: list[Path],
     timezone: str,
