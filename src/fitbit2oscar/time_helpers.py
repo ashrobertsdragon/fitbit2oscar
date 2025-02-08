3563696d9db249d7178c03d0387f57642bdef401
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-08 09:09:46 -0700

    move date & time related functions to own module

---
+++ src/fitbit2oscar/time_helpers.py
@@ -0,0 +1,65 @@
+import datetime
+import time
+from pathlib import Path
+from zoneinfo import ZoneInfo
+
+from fitbit2oscar.read_file import read_csv_file
+from fitbit2oscar.takeout.paths import profile_path
+
+
+def convert_timestamp(
+    timestamp: str,
+    timezone: str | None = None,
+    timestamp_format: str = "%Y-%m-%dT%H:%M:%S",
+) -> datetime.datetime:
+    """
+    Parse timestamp and attach timezone info, converting from UTC if needed.
+
+    Args:
+        timestamp (str): Timestamp in ISO 8601 format, optionally ending with
+            'Z' for UTC.
+        timezone (str): Timezone to convert timestamps to
+            (e.g., 'America/New_York').
+        time_string_format (str, optional): Format of the timestamp string.
+            Defaults to "%Y-%m-%dT%H:%M:%S".
+
+    Returns:
+        datetime.datetime: Timezone-aware datetime object in the target
+            timezone.
+    """
+    dt = datetime.datetime.strptime(
+        timestamp.removesuffix("Z"), timestamp_format
+    )
+    tz = ZoneInfo(timezone)
+
+    return (
+        dt.replace(tzinfo=datetime.timezone.utc).astimezone(tz)
+        if timestamp.endswith("Z")
+        else dt.replace(tzinfo=tz)
+    )
+
+
+def convert_time_data(minutes: int) -> str:
+    """Converts time in minutes to a string in HH:MM:SS format."""
+    hours, mins = divmod(minutes, 60)
+    return f"{hours:02d}:{mins:02d}:00"
+
+
+def get_local_timezone() -> datetime.timezone:
+    """Determine local timezone."""
+    local = time.localtime()
+    utc = time.gmtime()
+
+    day_offset = (utc.tm_yday - local.tm_yday) * 24
+    offset = local.tm_hour - utc.tm_hour + day_offset
+
+    return datetime.timezone(datetime.timedelta(hours=offset))
+
+
+def get_timezone_from_profile(fitbit_path: Path) -> str:
+    """Get timezone from profile file."""
+    for row in read_csv_file(profile_path(fitbit_path)):
+        timezone: str = row["timezone"]
+    if not timezone:
+        raise ValueError("Could not find timezone")
+    return timezone
