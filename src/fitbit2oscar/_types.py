16735fca2ab56173401366ce22e557dd569ae8de
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-08 09:10:13 -0700

    move TypeAlias's to own module

---
+++ src/fitbit2oscar/_types.py
@@ -0,0 +1,6 @@
+from typing import TypeAlias
+
+SleepSummary: TypeAlias = dict[str, dict[str, int]]
+SleepData: TypeAlias = list[dict[str, str | int]]
+SleepLevels: TypeAlias = dict[str, SleepSummary | SleepData]
+SleepEntry: TypeAlias = dict[str, str | int | SleepLevels]
