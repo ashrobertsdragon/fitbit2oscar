54154a3b3cc2a7c03612a443cb235bed0246a75b
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-11 21:46:04 -0700

    Switch to logging from loguru and add package selection

--- src/fitbit2oscar/__main__.py
+++ src/fitbit2oscar/__main__.py
@@ -1,13 +1,13 @@
 import argparse
 import datetime
-import sys
+import logging
 from pathlib import Path
 
-from loguru import logger
-
-import fitbit2oscar.helpers
+import fitbit2oscar.helpers as helper
 import fitbit2oscar.parse
 import fitbit2oscar.write_file
+
+logger = logging.getLogger(__name__)
 
 
 def process_data(args: argparse.Namespace) -> None:
@@ -17,8 +17,9 @@ def process_data(args: argparse.Namespace) -> None:
     args.export_path.mkdir(parents=True, exist_ok=True)
 
     viatom_data, dreem_data = fitbit2oscar.helpers.get_data(args)
+    viatom_chunks = fitbit2oscar.helpers.chunk_viatom_data(viatom_data)
 
-    fitbit2oscar.write_file.create_viatom_file(args.output_path, viatom_data)
+    fitbit2oscar.write_file.create_viatom_file(args.output_path, viatom_chunks)
     fitbit2oscar.write_file.write_dreem_file(args.output_path, dreem_data)
 
     finish_message = (
@@ -27,44 +28,106 @@ def process_data(args: argparse.Namespace) -> None:
     logger.info(finish_message)
 
 
-def configure_logger(args) -> None:
+def configure_logger(args: argparse.Namespace) -> None:
     """Sets up logger with verbose and log file options."""
-    logger.remove()
-
-    format_str = (
-        "{time} {level} {message}" if args.level else "{time} {message}"
+    format = (
+        "%(asctime)s - %(levelname)s - %(message)s"
+        if args.level
+        else "%(asctime)s - %(message)s"
     )
-    level = args.level or "WARNING"
+    logging.basicConfig(
+        format=format,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[logging.StreamHandler(), logging.FileHandler(args.log_file)]
+        if args.log_file
+        else [logging.StreamHandler()],
+    )
+    logger.setLevel(getattr(logging, args.level))
 
-    sink = args.logfile if args.logfile else sys.stderr
-    logger.add(sink=sink, format=format_str, level=level)
+
+class PackageVersionAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string) -> None:
+        package_map = {
+            "-H": "health_sync",
+            "-T": "takeout",
+            "--health_sync": "health_sync",
+            "--takeout": "takeout",
+        }
+        namespace.input_type = package_map[option_string]
+        path = helper.get_fitbit_path(
+            input_path=values, input_type=namespace.input_type
+        )
+        setattr(namespace, self.dest, path)
 
 
-def main() -> None:
+class StoreLogFile(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None) -> None:
+        if not getattr(namespace, "verbosity", 0):
+            logger.error(
+                "Verbosity must be set to at least 'INFO' to log to a file."
+            )
+            parser.error(
+                f"{option_string} requires a verbosity level to be set (e.g., --verbose, --very-verbose, -v, -vv)."
+            )
+
+        setattr(namespace, self.dest, values)
+
+
+def create_parser() -> argparse.Namespace:
+    """Create an argument parser for the command line interface."""
     parser = argparse.ArgumentParser(
         prog="Fitbit to OSCAR Data Converter",
         description="Converts Fitbit data to OSCAR format",
     )
 
+    input = parser.add_argument_group("Input path options")
+    input_path = input.add_mutually_exclusive_group(required=True)
+    input_path.add_argument(
+        "-T",
+        "--takeout-path",
+        help="Path to Takeout folder containing 'Fitbit' or to Takeout folder",
+        action=PackageVersionAction,
+        dest="fitbit_path",
+    )
+    input_path.add_argument(
+        "-H",
+        "--health-sync-path",
+        help="Path to Health Sync folder",
+        action=PackageVersionAction,
+        dest="fitbit_path",
+    )
+
     parser.add_argument(
+        "-o",
+        "--export-path",
+        help="Path to export files to, defaults to 'export' in current directory",
+        type=Path,
+        default=Path("export"),
+    )
+
+    dates = parser.add_argument_group("Date options")
+    dates.add_argument(
         "-s",
         "--start-date",
         metavar="<YYYY-M-D>",
-        type=lambda x: fitbit2oscar.helpers.process_date_arg(x, "start"),
+        type=helper.process_date_arg,
         help="Optional start date for data",
         default=datetime.date(2010, 1, 1),
     )
 
-    parser.add_argument(
+    dates.add_argument(
         "-e",
         "--end-date",
         metavar="<YYYY-M-D>",
-        type=lambda x: fitbit2oscar.helpers.process_date_arg(x, "end"),
+        type=helper.process_date_arg,
         help="Optional end date for data",
         default=datetime.date.today(),
     )
 
-    parser.add_argument(
+    verbosity = parser.add_argument_group(title="Logging options")
+
+    verbosity_args = verbosity.add_mutually_exclusive_group()
+    verbosity_args.add_argument(
         "-v",
         "--verbose",
         help="Set verbose logging",
@@ -72,7 +135,7 @@ def main() -> None:
         const="INFO",
         dest="level",
     )
-    parser.add_argument(
+    verbosity.add_argument(
         "-vv",
         "--very-verbose",
         help="Set verbose logging",
@@ -80,28 +143,21 @@ def main() -> None:
         const="DEBUG",
         dest="level",
     )
-    parser.add_argument(
+    verbosity.add_argument(
         "-l",
         "--logfile",
         metavar="<filename.log>",
-        help="Log to file instead of stderr",
+        help="Log to file instead of stderr. Logging must be set to verbose or very verbose",
+        dest="log_file",
+        action=StoreLogFile,
+        type=Path,
     )
 
-    parser.add_argument(
-        "input-path",
-        help="Path to Takeout folder containing 'Fitbit' or to Takeout folder",
-        type=fitbit2oscar.helpers.get_fitbit_path,
-    )
+    return parser.parse_args()
 
-    parser.add_argument(
-        "-o",
-        "--export-path",
-        help="Path to export files to, defaults to 'export' in current directory",
-        type=lambda x: Path(x),
-        nargs="?",
-        default=Path("export"),
-    )
-    args = parser.parse_args()
+
+def main() -> None:
+    args = create_parser()
 
     configure_logger(args)
 
