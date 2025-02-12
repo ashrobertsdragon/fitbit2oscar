e9e75ccb23d49e37bb71f7dbaac44f487ac68019
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-11 21:46:25 -0700

    Add SleepHealthData type alias

--- src/fitbit2oscar/_types.py
+++ src/fitbit2oscar/_types.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from typing import TypeAlias
 
 SleepSummary: TypeAlias = dict[str, dict[str, int]]
 SleepData: TypeAlias = list[dict[str, str | int]]
 SleepLevels: TypeAlias = dict[str, SleepSummary | SleepData]
 SleepEntry: TypeAlias = dict[str, str | int | SleepLevels]
+
+SleepHealthData: TypeAlias = tuple[datetime, int, int]
