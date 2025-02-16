6de45be3122de5602fcb3ff6d075f47d04ac8805
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-16 12:13:44 -0700

    Keep data in generators

--- src/fitbit2oscar/process_data.py
+++ src/fitbit2oscar/process_data.py
@@ -1,9 +1,10 @@
 import argparse
 import logging
+from collections.abc import Generator
 from datetime import datetime
 
 from fitbit2oscar import write_file
-from fitbit2oscar._types import SleepHealthData
+from fitbit2oscar._types import SleepHealthData, SleepEntry, VitalsData
 from fitbit2oscar.factory import DataHandlerFactory
 from fitbit2oscar.parsers import parse_sleep_data, parse_sleep_health_data
 
@@ -13,13 +14,17 @@ logger = logging.getLogger("fitbit2oscar")
 
 def get_data(
     args: argparse.Namespace,
-) -> tuple[list[list[SleepHealthData]], dict[str, datetime | int]]:
+) -> tuple[
+    Generator[VitalsData, None, None],
+    Generator[VitalsData, None, None],
+    Generator[SleepEntry, None, None],
+]:
     """Parse data using the appropriate handler."""
     handler = DataHandlerFactory.create_client(args.input_type, args)
     sp02_files, bpm_files, sleep_files, timezone = (
         handler.get_paths_and_timezone()
     )
-    sp02_data, bpm_data, sleep_data = handler.extract_data(
+    sp02_generator, bpm_generator, sleep_generator = handler.extract_data(
         sp02_files,
         bpm_files,
         sleep_files,
@@ -27,17 +32,25 @@ def get_data(
         args.start_date,
         args.end_date,
     )
+    return sp02_generator, bpm_generator, sleep_generator
+
+
+def parse_data(
+    sp02_generator: Generator[VitalsData],
+    bpm_generator: Generator[VitalsData],
+    sleep_generator: Generator[SleepEntry],
+):
     viatom_data: list[list[SleepHealthData]] = parse_sleep_health_data(
-        sp02_data, bpm_data
+        sp02_generator, bpm_generator
     )
-    dreem_data: dict[str, datetime | int] = parse_sleep_data(sleep_data)
+    dreem_data: dict[str, datetime | int] = parse_sleep_data(sleep_generator)
     return viatom_data, dreem_data
 
 
 def chunk_viatom_data(
     viatom_data: list[list[SleepHealthData]],
     chunk_size: int = 4095,
-) -> list[list[SleepHealthData]]:
+) -> Generator[list[SleepHealthData], None, None]:
     """
     Break up viatom data into chunks of size chunk_size.
 
@@ -47,19 +60,15 @@ def chunk_viatom_data(
             containing timestamps, sp02, and BPM values.
         chunk_size (int, optional): Maximum chunk size. Defaults to 4095.
 
-    Returns:
-        list[list[tuple[datetime, int, int]]]: List of data chunks.
+    yields:
+        list[SleepHealthData]: List of sleep health data tuples, up to chunk_size in length for each session.
     """
-    chunks = [
-        session[i : i + chunk_size]
-        for session in viatom_data
-        for i in range(0, len(session), chunk_size)
-    ]
-    logger.info(
-        f"Chunked viatom data into {[len(chunk) for chunk in chunks]} "
-        f"chunks of size {chunk_size}"
-    )
-    return chunks
+    for i, session in enumerate(viatom_data, start=1):
+        logger.info(
+            f"Processing session {i} into {len(session // chunk_size)} chunks"
+        )
+        for j in range(0, len(session), chunk_size):
+            yield session[j : j + chunk_size]
 
 
 def process_data(args: argparse.Namespace) -> None:
@@ -68,7 +77,11 @@ def process_data(args: argparse.Namespace) -> None:
 
     args.export_path.mkdir(parents=True, exist_ok=True)
 
-    viatom_data, dreem_data = get_data(args)
+    sp02_generator, bpm_generator, sleep_generator = get_data(args)
+
+    viatom_data, dreem_data = parse_data(
+        sp02_generator, bpm_generator, sleep_generator
+    )
     viatom_chunks = chunk_viatom_data(viatom_data)
 
     write_file.create_viatom_file(args.output_path, viatom_chunks)
