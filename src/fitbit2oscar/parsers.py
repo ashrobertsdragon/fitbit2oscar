07f65a91ed50e8fd3671db8fbfd2b4dcaff259ae
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-16 19:44:11 -0700

    Convert parse_sleep_health_data into generator function

--- src/fitbit2oscar/parsers.py
+++ src/fitbit2oscar/parsers.py
@@ -1,55 +1,119 @@
 import datetime
-from collections.abc import Generator
+from collections.abc import Callable, Generator
+from functools import partial
 
 from fitbit2oscar.time_helpers import convert_time_data
-from fitbit2oscar._types import SleepData, SleepEntry, SleepHealthData
+from fitbit2oscar._types import (
+    SleepData,
+    SleepEntry,
+    SleepHealthData,
+    VitalsData,
+)
+
+
+def get_next_item(
+    item: Generator[VitalsData],
+) -> VitalsData:
+    """Returns the next item from a generator."""
+    return next(item)
+
+
+def next_sp02(
+    sp02_data: Generator[VitalsData, None, None], bpm: VitalsData
+) -> tuple[VitalsData, VitalsData]:
+    """Returns the next SpO2 and current BPM values."""
+    return get_next_item(sp02_data), bpm
+
+
+def next_bpm(
+    bpm_data: Generator[VitalsData, None, None], sp02: VitalsData
+) -> tuple[VitalsData, VitalsData]:
+    """Returns the next BPM and current SpO2 values."""
+    return sp02, get_next_item(bpm_data)
+
+
+def next_all(
+    sp02_data: Generator[VitalsData, None, None],
+    bpm_data: Generator[VitalsData, None, None],
+) -> tuple[VitalsData, VitalsData]:
+    """Returns the next SpO2 and BPM values."""
+    return get_next_item(sp02_data), get_next_item(bpm_data)
+
+
+def sync_timestamps(
+    sp02_data: Generator[VitalsData], bpm_data: Generator[VitalsData]
+) -> Generator[tuple[VitalsData, VitalsData], None, None]:
+    """
+    Synchronizes timestamps between SpO2 and BPM data.
+
+    This function takes generators of tuples containing timestamps and SpO2
+    and BPM values and synchronizes the timestamps between the two generators.
+    It yields tuples containing the synchronized timestamps and SpO2 and BPM
+    values.
+
+    Args:
+        sp02_data (Generator[VitalsData, None, None]): A generator of tuples
+            containing timestamps and SpO2 values.
+        bpm_data (Generator[VitalsData, None, None]): A generator of tuples
+            containing timestamps and BPM values.
+
+    Yields:
+        Generator[tuple[VitalsData, VitalsData], None, None]: Tuples
+            containing synchronized timestamps and SpO2 and BPM values.
+    """
+    sp02 = get_next_item(sp02_data)
+    bpm = get_next_item(bpm_data)
+
+    comparison: dict[
+        int,
+        tuple[Callable, Callable],
+    ] = {
+        -1: partial(next_sp02, sp02_data, bpm),
+        0: partial(next_all, sp02_data, bpm_data),
+        1: partial(next_bpm, bpm_data, sp02),
+    }
+
+    yield comparison[
+        (sp02.timestamp > bpm.timestamp) - (sp02.timestamp < bpm.timestamp)
+    ]()
 
 
 def parse_sleep_health_data(
-    sp02_data: dict[datetime.datetime, int],
-    bpm_data: dict[datetime.datetime, int],
+    sp02_data: Generator[VitalsData, None, None],
+    bpm_data: Generator[VitalsData, None, None],
     session_split: int = 15,
-) -> list[list[SleepHealthData]]:
+) -> Generator[list[SleepHealthData], None, None]:
     """
     Parses sleep health data into sessions.
 
-    This function takes dictionaries containing sp02 and BPM data for each
+    This function takes tuples containing sp02 and BPM data for each
     timestamp and splits them into sessions based on a specified session split
-    time. It returns a list of sessions, where each session is a list of tuples
+    time. It yields a session, where each session is a list of tuples
     containing timestamps, sp02, and BPM values.
 
     Args:
-        sp02_data (dict[datetime.datetime, int]): A dictionary containing sp02
-            data for each timestamp.
-        bpm_data (dict[datetime.datetime, int]): A dictionary containing BPM
-            data for each timestamp.
+        sp02_data (Generator[VitalsData, None, None]): A generator of tuples
+            containing timestamps and sp02 values.
+        bpm_data (Generator[VitalsData, None, None]): A generator of tuples
+            containing timestamps and BPM values.
         session_split (int, optional): The session split time in minutes.
             Defaults to 15.
 
     Returns:
-        list[list[SleepHealthData]]: A list of sessions, where each session is
-            a list of tuples containing timestamps, sp02, and BPM values.
+        Generator[list[SleepHealthData], None, None]: A generator of sessions
     """
-    sessions = []
-    session = []
 
+    session: list[SleepHealthData] = []
     prev_timestamp = None
 
-    for timestamp in sorted(sp02_data.keys()).intersection(bpm_data.keys()):
-        session.append((timestamp, sp02_data[timestamp], bpm_data[timestamp]))
-        if (
-            prev_timestamp
-            and prev_timestamp + datetime.timedelta(minutes=session_split)
-            < timestamp
-        ):
-            sessions.append(session)
+    for sp02, bpm in sync_timestamps(sp02_data, bpm_data):
+        session.append((sp02.timestamp, sp02.data, bpm.data))
+        if prev_timestamp and (
+            sp02.timestamp - prev_timestamp
+        ) > datetime.timedelta(minutes=session_split):
+            yield session
             session.clear()
-        prev_timestamp = timestamp
-
-    if session:
-        sessions.append(session)
-
-    return sessions
+        prev_timestamp = sp02.timestamp
 
 
 def generate_hypnogram(data: SleepData) -> list[str]:
