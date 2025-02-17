91cc94191e3fd69d04fcd65a252c2fbcf504facc
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-17 15:53:51 -0700

    Add logging

--- src/fitbit2oscar/factory.py
+++ src/fitbit2oscar/factory.py
@@ -1,7 +1,10 @@
 import argparse
+import logging
 
 from fitbit2oscar.exceptions import FItbitConverterValueError
 from fitbit2oscar.handlers import DataHandler
+
+logger = logging.getLogger("fitbit2oscar")
 
 
 class DataHandlerFactory:
@@ -12,6 +15,7 @@ class DataHandlerFactory:
         try:
             return DataHandler._registry[input_type](args)
         except KeyError:
+            logging.error(f"Invalid input type '{input_type}'")
             raise FItbitConverterValueError(
                 f"Invalid input type '{input_type}'"
             )
