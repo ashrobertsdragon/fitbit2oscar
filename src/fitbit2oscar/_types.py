9a1f45b0637738372beed5d67cd7903bd4180850
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-21 14:30:47 -0700

    Add Vitals and nest sleep inside config class

--- src/fitbit2oscar/_types.py
+++ src/fitbit2oscar/_types.py
@@ -1,7 +1,7 @@
 from collections.abc import Callable
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Generic, TypeAlias, TypeVar, NamedTuple
+from typing import TypeAlias, TypeVar, NamedTuple
 
 DictNotation: TypeAlias = list[str] | str
 
@@ -23,7 +23,7 @@ class VitalsData(NamedTuple):
 
 
 @dataclass
-class SleepKeys(Generic[Sleep]):
+class Keys:
     timestamp: str
     start_time: str
     time_awake_after_sleep_onset: str
@@ -36,15 +36,31 @@ class SleepKeys(Generic[Sleep]):
 
 
 @dataclass
-class SourceFormat(Generic[Sleep]):
-    sleep_keys: SleepKeys[Sleep] = field(default_factory=SleepKeys)
-    transforms: (
+class SleepConfig:
+    keys: Keys
+    sleep_transformations: (
         dict[str, Callable[[Sleep], str | int | SleepLevels]] | None
     ) = None
-    required_fields: list[DictNotation]
 
     def __post_init__(self) -> None:
-        if self.transforms is None:
-            self.transforms = {
-                key: value for key, value in self.sleep_keys.items()
+        if self.sleep_transformations is None:
+            self.sleep_transformations = {
+                key: value for key, value in self.keys.items()
             }
+
+
+@dataclass
+class VitalsConfig:
+    timestamp: str
+    spo2: str
+    bpm: str
+
+
+@dataclass
+class Config:
+    required_fields: list[DictNotation] = field(default_factory=list)
+    timezone: str | None = None
+    use_seconds: bool = field(default=True)
+
+    sleep: SleepConfig = field(default_factory=SleepConfig)
+    vitals: VitalsConfig = field(default_factory=VitalsConfig)
