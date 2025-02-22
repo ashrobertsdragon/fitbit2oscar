660c1f1fa017982ad68bd7449d51197b037162be
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-21 21:44:46 -0700

    move config class to own module

--- src/fitbit2oscar/_types.py
+++ src/fitbit2oscar/_types.py
@@ -1,8 +1,5 @@
-from collections.abc import Callable
 from datetime import datetime
 from typing import TypeAlias, TypeVar, NamedTuple
-
-from pydantic import BaseModel, Field
 
 DictNotation: TypeAlias = list[str] | str
 
@@ -21,43 +18,3 @@ Sleep = TypeVar("Sleep", CSVRows, SleepEntry)
 class VitalsData(NamedTuple):
     timestamp: datetime.datetime
     data: int
-
-
-class Keys(BaseModel):
-    timestamp: str
-    start_time: str
-    time_awake_after_sleep_onset: str
-    duration: str | None = None
-    stop_time: str | None = None
-    sleep_efficiency: str | None = None
-
-    sleep_stages: DictNotation
-    summary: DictNotation | None = None
-
-
-class SleepConfig(BaseModel):
-    keys: Keys
-    sleep_transformations: (
-        dict[str, Callable[[Sleep], str | int | SleepLevels]] | None
-    ) = None
-
-    def __post_init__(self) -> None:
-        if self.sleep_transformations is None:
-            self.sleep_transformations = {
-                key: value for key, value in self.keys.items()
-            }
-
-
-class VitalsConfig(BaseModel):
-    timestamp: str
-    spo2: str
-    bpm: str
-
-
-class Config(BaseModel):
-    required_fields: list[DictNotation] = Field(default_factory=list)
-    timezone: str | None = None
-    use_seconds: bool = Field(default=True)
-
-    sleep: SleepConfig = Field(default_factory=SleepConfig)
-    vitals: VitalsConfig = Field(default_factory=VitalsConfig)
