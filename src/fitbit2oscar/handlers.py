10ad4112dddbc7cf38609fbf5b6ebac5c60ea6ba
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-22 07:01:51 -0700

    DataHandler now only class in module

--- src/fitbit2oscar/handlers.py
+++ src/fitbit2oscar/handlers.py
@@ -1,9 +1,11 @@
 import argparse
-import datetime
 import importlib
 from pathlib import Path
+from collections.abc import Generator
 
-import fitbit2oscar.time_helpers as time_helpers
+from fitbit2oscar.config import Config
+from fitbit2oscar.exceptions import FitbitConverterDataError
+from fitbit2oscar._types import VitalsData, SleepEntry
 
 
 class DataHandler:
@@ -19,17 +21,15 @@ class DataHandler:
         cls._registry[cls.package] = cls
 
     def __repr__(cls) -> str:
-        return f"{cls.__name__} handler for {cls.package}input type"
+        return f"{cls.__name__} handler for {cls.package} input type"
 
-    def __init__(self, args: argparse.Namespace) -> None:
+    def __init__(self, args: argparse.Namespace, config: Config) -> None:
         self.args = args
+        self.config = config
         package = (
             f"{DataHandler.package}.{self.package}"
             if issubclass(self.__class__, DataHandler)
             else self.package
-        )
-        self.paths_module: importlib.ModuleType = importlib.import_module(
-            f"{package}.paths"
         )
         self.extractor_module: importlib.ModuleType = importlib.import_module(
             f"{package}.extract"
@@ -40,54 +40,52 @@ class DataHandler:
             "sp02_paths": [],
             "bpm_paths": [],
         }
-        self.timezone: datetime.timezone | None = None
 
-    def get_paths_and_timezone(self) -> None:
+    @property
+    def profile_path(self) -> Path:
+        if self.config.profile_path is None:
+            raise FitbitConverterDataError(
+                f"There is no profile path utilized in "
+                f"{self.package} input type"
+            )
+        return self.args.fitbit_path / self._walk_paths(
+            self.config.profile_path
+        )
+
+    @staticmethod
+    def _walk_paths(paths: list[str] | str):
+        return paths if isinstance(paths, str) else "/".join(paths)
+
+    def _build_glob_pattern(self, **kwargs) -> str:
+        """Build glob pattern based on provided arguments"""
+        raise NotImplementedError
+
+    def _dirs(self) -> tuple[Path, Path, Path]:
         """Get paths and timezone."""
+        spo2_dir = self.args.fitbit_path / self._parse_dict_notation(
+            self.config.vitals.spo2_dir
+        )
+        bpm_dir = self.args.fitbit_path / self._parse_dict_notation(
+            self.config.vitals.bpm_dir
+        )
+        sleep_dir = self.args.fitbit_path / self._parse_dict_notation(
+            self.config.sleep.dir
+        )
+        return spo2_dir, bpm_dir, sleep_dir
+
+    def get_paths(self) -> None:
+        """Get list of paths for files for SpO2, heart rate, and sleep data"""
         raise NotImplementedError
 
     def parse_data(
         self,
-    ) -> tuple[list[dict[str, datetime.datetime | int]], list]:
-        sp02_data, bpm_data = self.extractor_module.extract_sleep_health_data(
-            self.paths["sp02_paths"],
-            self.paths["bpm_paths"],
-            self.timezone,
+    ) -> tuple[
+        Generator[VitalsData, None, None],
+        Generator[VitalsData, None, None],
+        Generator[SleepEntry, None, None],
+    ]:
+        return self.extractor_module.extract_data(
+            self.paths,
             self.args.start_date,
             self.args.end_date,
         )
-        sleep_data = self.extractor_module.extract_sleep_data(
-            self.paths["sleep_paths"],
-            self.timezone,
-            self.args.start_date,
-            self.args.end_date,
-        )
-        return sp02_data, bpm_data, sleep_data
-
-
-class TakeoutHandler(DataHandler):
-    package = "takeout"
-
-    def get_paths_and_timezone(self):
-        (
-            self.paths["sleep_paths"],
-            self.paths["sp02_paths"],
-            self.paths["bpm_paths"],
-        ) = self.paths_module.get_paths(self.args.fitbit_path)
-        self.timezone = time_helpers.get_timezone_from_profile(
-            self.args.fitbit_path
-        )
-
-
-class HealthSyncHandler(DataHandler):
-    package: str = "health_sync"
-
-    def get_paths_and_timezone(self):
-        (
-            self.paths["sleep_paths"],
-            self.paths["sp02_paths"],
-            self.paths["bpm_paths"],
-        ) = self.paths_module.get_paths(
-            self.args.fitbit_path, self.args.date_format
-        )
-        self.timezone = time_helpers.get_local_timezone()
