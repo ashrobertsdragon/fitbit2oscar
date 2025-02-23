08108bb4e66a54ec9bf927fe4c621de84741b994
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-02-22 22:26:13 -0700

    Add resolver and call automatically with transform dict

--- src/fitbit2oscar/config.py
+++ src/fitbit2oscar/config.py
@@ -1,50 +1,81 @@
 from collections.abc import Callable
+from typing import TypedDict
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 from fitbit2oscar._types import DictNotation, Sleep, SleepLevels
 
+datetime_format = "%Y.%m.%d %H:%M:%S"
+date_format = "%Y.%m.%d"
+time_format = "%H:%M"
 
-class SleepKeys(BaseModel):
+
+class Resolver:
+    _computed: dict = {}
+
+    def resolve(self, entry: Sleep, key: str) -> str | int | SleepLevels:
+        """Ensure transformation function arguments exist when called"""
+        if key not in self._computed:
+            self._computed[key] = self.sleep_transformations[key](entry)
+        return self._computed[key]
+
+    def clear(self) -> None:
+        self._computed.clear()
+
+
+class SleepKeys(TypedDict):
     timestamp: str
     start_time: str
-    time_awake_after_sleep_onset_duration: str
+    time_awake_after_sleep_onset_duration: str | None = None
     duration: str | None = None
     stop_time: str | None = None
     sleep_efficiency: str | None = None
+    levels: str | None = None
 
     sleep_stages: DictNotation
     summary: DictNotation | None = None
 
 
 class SleepConfig(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     glob: str
     filetype: str
     keys: SleepKeys
     sleep_transformations: (
         dict[str, Callable[[Sleep], str | int | SleepLevels]] | None
     ) = None
+    resolver: Resolver | None = None
 
-    def __post_init__(self) -> None:
+    def model_post_init(self) -> None:
         if self.sleep_transformations is None:
             self.sleep_transformations = {
-                key: value for key, value in self.keys.items()
+                key: lambda entry: entry[key] for key in self.keys.values()
             }
 
+    def _reset_before_entry(self):
+        self.resolver.clear()
 
-class VitalsConfig(BaseModel):
+    def __getattr__(self, item):
+        if self.resolver and item == "sleep_transformations":
+            self._reset_before_entry()
+        return super().__getattr__(item)
+
+
+class VitalsConfig(TypedDict):
     timestamp: str
-    spo2: str
-    bpm: str
+    spo2_key: str
+    bpm_key: str
     spo2_glob: str
     bpm_glob: str
     spo2_filetype: str
     bpm_filetype: str
+    spo2_dir: str
+    bpm_dir: str
 
 
 class Config(BaseModel):
     required_fields: list[DictNotation] = Field(default_factory=list)
-    timezone: str | None = None
     profile_path: str | None = None
     use_seconds: bool = Field(default=True)
 
