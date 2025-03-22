0e3e971316771a5905c50741e4a0772eab7af4fb
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-03-22 00:35:53 -0600

    add config fetch to factory

--- src/fitbit2oscar/factory.py
+++ src/fitbit2oscar/factory.py
@@ -2,20 +2,24 @@ import argparse
 import importlib
 import contextlib
 import pkgutil
+import sys
 
 import fitbit2oscar.plugins
+from fitbit2oscar.config import Config
 from fitbit2oscar.exceptions import FitbitConverterValueError
 from fitbit2oscar.handlers import DataHandler
 from fitbit2oscar._logger import logger
 
+PLUGINS_DIR = fitbit2oscar.plugins
 
-for _, name, is_package in pkgutil.walk_packages(
-    path=fitbit2oscar.plugins.__path__,
-):
-    if is_package:
-        with contextlib.suppress(ModuleNotFoundError):
-            importlib.import_module(f"fitbit2oscar.plugins.{name}.handler")
-            logger.debug(f"Plugin {name} added")
+with contextlib.suppress(ModuleNotFoundError):
+    plugins = {
+        name: importlib.import_module(f"{PLUGINS_DIR.__name__}.{name}.handler")
+        for _, name, is_package in pkgutil.walk_packages(
+            path=PLUGINS_DIR.__path__
+        )
+        if is_package
+    }
 
 
 class DataHandlerFactory:
@@ -23,9 +27,20 @@ class DataHandlerFactory:
     def create_client(
         input_type: str, args: argparse.Namespace
     ) -> DataHandler:
+        plugin = plugins[input_type]
+        for obj in vars(plugin).values():
+            if isinstance(obj, Config):
+                config = obj
+                break
+        else:
+            logger.error(f"{input_type} is not a valid plugin")
+            raise FitbitConverterValueError(
+                f"{input_type} is not a valid plugin"
+            )
+
         try:
-            return DataHandler._registry[input_type](args)
-        except KeyError:
+            return DataHandler._registry[input_type](args, config)
+        except (KeyError, UnboundLocalError):
             logger.error(f"Invalid input type '{input_type}'")
             raise FitbitConverterValueError(
                 f"Invalid input type '{input_type}'"
