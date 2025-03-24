19f947583381e69556e381b13511d1c6df382ec9
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-03-23 21:50:52 -0600

    remove default strptime format

--- src/fitbit2oscar/time_helpers.py
+++ src/fitbit2oscar/time_helpers.py
@@ -2,7 +2,6 @@ import datetime
 import json
 import time
 from pathlib import Path
-from zoneinfo import ZoneInfo
 
 from fitbit2oscar.exceptions import (
     FitbitConverterValueError,
@@ -21,8 +20,8 @@ def calculate_duration(
 
 def convert_timestamp(
     timestamp: str,
-    timezone: str | None = None,
-    timestamp_format: str = "%Y-%m-%dT%H:%M:%S",
+    timestamp_format: str,
+    timezone: datetime.timezone | None = None,
     use_seconds: bool = True,
 ) -> datetime.datetime:
     """
@@ -34,16 +33,15 @@ def convert_timestamp(
         timezone (str): Timezone to convert timestamps to
             (e.g., 'America/New_York').
         time_string_format (str, optional): Format of the timestamp string.
-            Defaults to "%Y-%m-%dT%H:%M:%S".
 
     Returns:
         datetime.datetime: Timezone-aware datetime object in the target
             timezone.
     """
     dt = datetime.datetime.strptime(
-        timestamp.removesuffix("Z"), timestamp_format
+        timestamp.replace("T", " ").removesuffix("Z"), timestamp_format
     )
-    tz = ZoneInfo(timezone) if timezone else get_local_timezone()
+    tz = timezone or get_local_timezone()
 
     local_dt = (
         dt.replace(tzinfo=datetime.timezone.utc).astimezone(tz)
